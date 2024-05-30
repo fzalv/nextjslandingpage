abde9e7a994be5466595c7d9c6561ec561a36d6d
fzalv <youhaveamessag3@gmail.com>
2024-05-30 23:04:36 +0700

    Guide section

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,7 +1,63 @@
+import Image from "next/image";
 import React from "react";
 
 const Guide = () => {
-  return <div>Guide</div>;
+  return (
+    <section className="flexCenter flex-col">
+      <div className="border-2 border-red-500 padding-container max-container w-full pb-24">
+        <Image src="/camp.svg" alt="camp" width={50} height={50} />
+        <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
+          We are here for you
+        </p>
+        <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[520px]">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos quae
+            harum vel eius sunt consequuntur repellat, placeat iste temporibus
+            voluptates, reiciendis illo enim unde velit iusto quas ut est eaque
+            ratione quam ab quos dolorem et fuga? Voluptate reiciendis quaerat
+            ratione obcaecati libero eos ea, veritatis eveniet inventore
+            reprehenderit adipisci.
+          </h2>
+        </div>
+      </div>
+
+      <div className="flexCenter max-container relative w-full border-2 border-blue-500">
+        <Image
+          src="/boat.png"
+          alt="boat"
+          width={1440}
+          height={580}
+          className="w-full object-cover object-center 2xl:rounded-5xl"
+        />
+
+        <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+          <Image
+            src="/meter.svg"
+            alt="meter"
+            width={16}
+            height={158}
+            className="h-full w-auto"
+          />
+          <div className="flexBetween flex-col">
+            <div className="flex w-full flex-col">
+              <div className="flexBetween w-full">
+                <p className="regular-16 text-gray-20">Destination</p>
+                <p className="bold-16 text-green-50">48 min</p>
+              </div>
+              <p className="bold-20 mt-20">Agusto Fernandes</p>
+            </div>
+
+            <div className="flex w-full flex-col">
+              <p className="regular-16 text-gray-20">Start Track</p>
+              <h4 className="bold-20 mt-2 whitespace-nowrap">
+                Cianjur Selatan
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Guide;
